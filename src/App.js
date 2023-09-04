7c590136b46cfb5f04ec5feec880a5fdfb0f399f
MahdiT54 <mahditanzim2@gmail.com>
2023-09-04 02:15:09 -0400

    login screen initialized

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import './App.css';
-import HomeScreen from './HomeScreen';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import React from "react";
+import "./App.css";
+import HomeScreen from "./screens/HomeScreen";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginScreen from "./screens/LoginScreen";
 
 function App() {
+  const user = null;
   return (
     <div className="app">
-      <HomeScreen />
+      <BrowserRouter>
+        {!user ? (
+          <LoginScreen />
+        ) : (
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
+        )}
+      </BrowserRouter>
     </div>
   );
 }
