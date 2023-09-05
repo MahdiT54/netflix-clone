e34298117beb61d838db82b9ceda45a5c411efe0
MahdiT54 <mahditanzim2@gmail.com>
2023-09-05 13:01:42 -0400

    having form connection issues

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+
 const firebaseConfig = {
   apiKey: "AIzaSyCwfZEQ0AcRQuQiJmSMt6EZ2bAwb_7x1iY",
   authDomain: "netflix-clone-ee113.firebaseapp.com",
@@ -7,3 +11,11 @@ const firebaseConfig = {
   messagingSenderId: "1024421897798",
   appId: "1:1024421897798:web:24bafe47c9836e22738c47",
 };
+
+// Initialize Firebase app
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore();
+const auth = getAuth();
+
+export { auth, createUserWithEmailAndPassword };
+export default db;
