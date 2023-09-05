d4fb7699c92ea934bc3f829307ebd93ffdae0608
MahdiT54 <mahditanzim2@gmail.com>
2023-09-04 20:38:36 -0400

    loginScreen activated

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "./LoginScreen.css";
+import NavLogo from "../assets/navlogo.png";
 
 const LoginScreen = () => {
-  return <div className="loginScreen"></div>;
+  return (
+    <div className="loginScreen">
+      <div className="loginScreen__background">
+        <img className="loginScreen__logo" src={NavLogo} alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default LoginScreen;
