1b742a363b777a57237bc5d358d5b9a5d6d3b26d
Mahdi <mahditanzim2@gmail.com>
2023-09-07 14:41:39 -0400

    update

--- src/App.js
+++ src/App.js
@@ -4,24 +4,31 @@ import HomeScreen from "./screens/HomeScreen";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginScreen from "./screens/LoginScreen";
 import { auth } from "./firebase";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/userSlice";
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
   let unsubscribe;
 
   useEffect(() => {
     unsubscribe = auth.onAuthStateChanged((userAuth) => {
       if (userAuth) {
-        console.log(userAuth);
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        );
       } else {
         //Logged out
+        dispatch(logout);
       }
     });
 
-    
     return unsubscribe;
   }, []);
-
 
   return (
     <div className="app">
@@ -30,6 +37,9 @@ function App() {
           <LoginScreen />
         ) : (
           <Routes>
+            <Route>
+              
+            </Route>
             <Route path="/" element={<HomeScreen />} />
           </Routes>
         )}
