69d85c4babff727885b8cc3844bf5d270c218bd9
Mahdi <mahditanzim2@gmail.com>
2023-09-09 02:09:21 -0400

    finished

--- src/screens/PlansScreen.js
+++ src/screens/PlansScreen.js
@@ -18,6 +18,33 @@ import { loadStripe } from "@stripe/stripe-js";
 const PlansScreen = () => {
   const [products, setProducts] = useState([]);
   const user = useSelector(selectUser);
+  const [subscription, setSubscription] = useState(null);
+
+  useEffect(() => {
+    const fetchSubscription = async () => {
+      try {
+        const subscriptionsRef = collection(
+          db,
+          `customers/${user.uid}/subscriptions`
+        );
+        const querySnapshot = await getDocs(subscriptionsRef);
+
+        querySnapshot.forEach((subscriptionDoc) => {
+          setSubscription({
+            role: subscriptionDoc.data().role,
+            current_period_end:
+              subscriptionDoc.data().current_period_end.seconds,
+            current_period_start:
+              subscriptionDoc.data().current_period_start.seconds,
+          });
+        });
+      } catch (error) {
+        console.error("Error fetching subscription:", error);
+      }
+    };
+
+    fetchSubscription();
+  }, [user.uid]);
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -58,54 +85,84 @@ const PlansScreen = () => {
   }, []);
 
   console.log(products);
+  console.log(subscription);
 
   const loadCheckout = async (priceId) => {
-  if (!priceId) {
-    console.error("priceId is undefined or null");
-    return;
-  }
+    if (!priceId) {
+      console.error("priceId is undefined or null");
+      return;
+    }
 
-  const checkoutSessionsRef = collection(db, "customers", user.uid, "checkout_sessions");
+    const checkoutSessionsRef = collection(
+      db,
+      "customers",
+      user.uid,
+      "checkout_sessions"
+    );
 
-  try {
-    const docRef = await addDoc(checkoutSessionsRef, {
-      price: priceId,
-      success_url: window.location.origin,
-      cancelUrl: window.location.origin,
-    });
+    try {
+      const docRef = await addDoc(checkoutSessionsRef, {
+        price: priceId,
+        success_url: window.location.origin,
+        cancelUrl: window.location.origin,
+      });
 
-    onSnapshot(docRef, async (snap) => {
-      const { error, sessionId } = snap.data();
+      onSnapshot(docRef, async (snap) => {
+        const { error, sessionId } = snap.data();
 
-      if (error) {
-        alert(`Woah! There seems to be an error on our end: ${error.message}`);
-      }
-      if (sessionId) {
-        const stripe = await loadStripe(
-          "pk_test_51Nny4iHFZPYcuaATz1Rk5Bs9kRhSftSNdyNZtlO0eS7m1RzNJWOh0QAinSJHyuxk3pj2OweG05MGWOiSTxCuboMU00j50ghBdp"
-        );
-        stripe.redirectToCheckout({ sessionId });
-      }
-    });
-  } catch (error) {
-    console.error("Error adding checkout session:", error);
-  }
-};
-
+        if (error) {
+          alert(
+            `Woah! There seems to be an error on our end: ${error.message}`
+          );
+        }
+        if (sessionId) {
+          const stripe = await loadStripe(
+            "pk_test_51Nny4iHFZPYcuaATz1Rk5Bs9kRhSftSNdyNZtlO0eS7m1RzNJWOh0QAinSJHyuxk3pj2OweG05MGWOiSTxCuboMU00j50ghBdp"
+          );
+          stripe.redirectToCheckout({ sessionId });
+        }
+      });
+    } catch (error) {
+      console.error("Error adding checkout session:", error);
+    }
+  };
 
   return (
     <div className="plansScreen">
-      {products.map((product) => (
-        <div key={product.id} className="plansScreen__plan">
-          <div className="plansScreen__info">
-            <h5>{product.data.name}</h5>
-            <h6>{product.data.description}</h6>
+      <br />
+      {subscription && (
+        <p>
+          Renewal date:{" "}
+          {new Date(
+            subscription?.current_period_end * 1000
+          ).toLocaleDateString()}
+        </p>
+      )}
+      {products.map((product) => {
+        const isCurrentPackage = product.data.name
+          ?.toLowerCase()
+          .includes(subscription?.role);
+        return (
+          <div
+            key={product.id}
+            className={`${
+              isCurrentPackage && "plansScreen__plan--disabled"
+            } plansScreen__plan`}
+          >
+            <div className="plansScreen__info">
+              <h5>{product.data.name}</h5>
+              <h6>{product.data.description}</h6>
+            </div>
+            <button
+              onClick={() =>
+                !isCurrentPackage && loadCheckout(product.prices[0].priceId)
+              }
+            >
+              {isCurrentPackage ? "Current Package" : "Subscribe"}
+            </button>
           </div>
-          <button onClick={() => loadCheckout(product.prices[0].priceId)}>
-            Subscribe
-          </button>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 };
